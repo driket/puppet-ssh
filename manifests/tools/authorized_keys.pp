88a901d3dd191405cadb13c1fc9728370f296ad4
Cedric Villa <cedric.villa@gmail.com>
2013-07-04 21:52:17 +0000

    secure ssh folder with proper unix priveleges

--- manifests/tools/authorized_keys.pp
+++ manifests/tools/authorized_keys.pp
@@ -5,14 +5,15 @@ define mv_ssh::tools::authorized_keys (	$keys 	= ["sh-rsa AZERTYUIOP..."],
 																			) 
 {
 	file {"${path}":
-		ensure => directory,
+		ensure 	=> directory,
+		mode 		=> "600", 
 	}
 	
 	file {"${path}/authorized_keys":
-		ensure => file,
-		owner => $user,
-		group => $group,
-		mode => "644",
+		ensure 	=> file,
+		owner 	=> $user,
+		group 	=> $group,
+		mode 		=> "600",
 		content => template("mv_ssh/authorized_keys.erb"),
 		require => File["${path}"],
 	}
