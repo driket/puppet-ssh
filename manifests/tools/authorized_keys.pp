2a4b59d48b73a3a30179fe5604e9ab80e274144d
Cedric Villa <cedric.villa@gmail.com>
2014-04-21 15:57:12 +0200

    add authorized keys tool

--- manifests/tools/authorized_keys.pp
+++ manifests/tools/authorized_keys.pp
@@ -1,20 +1,61 @@
-define mv_ssh::tools::authorized_keys (	$keys 	= ["ssh-rsa AZERTYUIOP..."], 
-																				$path 	= "/root/.ssh",
-																				$owner 	= "root",
-																				$group 	= "root",
-																			) 
+define mv_ssh::tools::authorized_keys (	
+	$keys 	= ["ssh-rsa AZERTYUIOP..."], 
+	$owner 	= "root",
+	$group 	= "root",
+) 
 {
-	file {"${path}":
-		ensure 	=> directory,
-		mode 		=> "0600", 
+	
+	case $owner {
+
+		'root': {
+
+			file {"/root":
+				ensure 	=> directory,
+			}
+			
+			file {"/root/.ssh":
+				ensure 	=> directory,
+				mode 		=> "0600", 
+				owner		=> $owner,
+				group 	=> $group,
+			}
+			
+			file {"/root/.ssh/authorized_keys":
+				ensure 	=> file,
+				owner 	=> $owner,
+				group 	=> $group,
+				mode 		=> "0600",
+				content => template("mv_ssh/authorized_keys.erb"),
+				require => File["/root/.ssh"],
+			}
+
+		}
+		
+    default: {
+			
+			file {"/home/${owner}":
+				ensure 	=> directory,
+			}
+			
+			file {"/home/${owner}/.ssh":
+				ensure 	=> directory,
+				mode 		=> "0600", 
+				owner		=> $owner,
+				group 	=> $group,
+			}
+			
+			file {"/home/${owner}/.ssh/authorized_keys":
+				ensure 	=> file,
+				owner 	=> $owner,
+				group 	=> $group,
+				mode 		=> "0600",
+				content => template("mv_ssh/authorized_keys.erb"),
+				require => File["/home/${owner}/.ssh"],
+			}
+      
+    }		
 	}
 	
-	file {"${path}/authorized_keys":
-		ensure 	=> file,
-		owner 	=> $owner,
-		group 	=> $group,
-		mode 		=> "0600",
-		content => template("mv_ssh/authorized_keys.erb"),
-		require => File["${path}"],
-	}
+
+
 }
