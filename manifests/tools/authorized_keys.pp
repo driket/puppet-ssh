3a4f161c97ec4dfae7d0d18ec069f3d610996a91
Cedric Villa <cedric.villa@gmail.com>
2014-05-06 15:04:40 +0200

    lint + remove name conflict

--- manifests/tools/authorized_keys.pp
+++ manifests/tools/authorized_keys.pp
@@ -1,61 +1,67 @@
-define mv_ssh::tools::authorized_keys (	
-	$keys 	= ["ssh-rsa AZERTYUIOP..."], 
-	$owner 	= "root",
-	$group 	= "root",
-) 
+define mv_ssh::tools::authorized_keys (
+  $keys   = ["ssh-rsa AZERTYUIOP..."],
+  $owner  = "root",
+  $group  = "root",
+)
 {
-	
-	case $owner {
 
-		'root': {
+  case $owner {
 
-			file {"/root":
-				ensure 	=> directory,
-			}
-			
-			file {"/root/.ssh":
-				ensure 	=> directory,
-				mode 		=> "0600", 
-				owner		=> $owner,
-				group 	=> $group,
-			}
-			
-			file {"/root/.ssh/authorized_keys":
-				ensure 	=> file,
-				owner 	=> $owner,
-				group 	=> $group,
-				mode 		=> "0600",
-				content => template("mv_ssh/authorized_keys.erb"),
-				require => File["/root/.ssh"],
-			}
+    'root': {
 
-		}
-		
+      file {'mv_ssh_home':
+        ensure  => directory,
+        name    => '/root',
+      }
+
+      file {'mv_ssh_home_ssh':
+        ensure  => directory,
+        name    => '/root/.ssh',
+        mode    => '0600',
+        owner   => $owner,
+        group   => $group,
+      }
+
+      file {'mv_ssh_authorized_keys':
+        ensure  => file,
+        name    => '/root/.ssh/authorized_keys',
+        owner   => $owner,
+        group   => $group,
+        mode    => '0600',
+        content => template('mv_ssh/authorized_keys.erb'),
+        require => File['mv_ssh_home_ssh'],
+      }
+
+    }
+
     default: {
-			
-			file {"/home/${owner}":
-				ensure 	=> directory,
-			}
-			
-			file {"/home/${owner}/.ssh":
-				ensure 	=> directory,
-				mode 		=> "0600", 
-				owner		=> $owner,
-				group 	=> $group,
-			}
-			
-			file {"/home/${owner}/.ssh/authorized_keys":
-				ensure 	=> file,
-				owner 	=> $owner,
-				group 	=> $group,
-				mode 		=> "0600",
-				content => template("mv_ssh/authorized_keys.erb"),
-				require => File["/home/${owner}/.ssh"],
-			}
-      
-    }		
-	}
-	
+
+      file {'mv_ssh_home':
+        ensure  => directory,
+        name    => "/home/${owner}",
+      }
+
+      file {'mv_ssh_home_ssh':
+        ensure  => directory,
+        name    => "/home/${owner}/.ssh",
+        mode    => '0600',
+        owner   => $owner,
+        group   => $group,
+      }
+
+      file {'mv_ssh_authorized_keys':
+        ensure  => file,
+        name    => "/home/${owner}/.ssh/authorized_keys",
+        owner   => $owner,
+        group   => $group,
+        mode    => '0600',
+        content => template('mv_ssh/authorized_keys.erb'),
+        require => File["/home/${owner}/.ssh"],
+      }
+
+    }
+  }
+
 
 
 }
